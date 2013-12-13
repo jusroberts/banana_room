0e62a9675b993c1472df7c77857486848a969a27
Justin Roberts <justin.roberts@careerbuilder.com>
2013-12-13 02:38:14 -0500

    haha... oops

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,5 +1,4 @@
-class PlayerController < ActionController::Base
+class PlayerController < ApplicationController
   def index
-    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BananaRoom::Application.routes.draw do
+  get "player/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
