f653d3d77fbe76ed05e0cfe567ed109123eec20f
Raspberry Spi <spi@cb.com>
2013-12-13 00:38:46 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BananaRoom::Application.config.secret_key_base = 'c91c8b9ac682a0821c7ce7e57ae6a2460b537cb7c208e20e0e335aa2540b89fef670666afa77f0e1bffb898533aa2cb03696ad5a0516017b5ca713b92a3b2ad5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BananaRoom::Application.config.session_store :cookie_store, key: '_banana_room_session'
